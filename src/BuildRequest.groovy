589c325ebae177da88e970d6ec9e08d8ffa93fec
Imran Ansari <imran.iansari@gmail.com>
2010-10-15 09:46:01 -0400

    first import

---
+++ src/BuildRequest.groovy
@@ -0,0 +1,15 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: imranansari
+ * Date: Oct 14, 2010
+ * Time: 2:07:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class BuildRequest {
+
+    String projectName;
+    String status;
+    String date;
+
+}
